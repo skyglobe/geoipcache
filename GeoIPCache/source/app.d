5d34d5fc9647783c15d12dcb2e5e23d521a368d6
Gianfranco Gallizia <skyglobe@skyglobe-studios.it>
2017-07-04 19:47:55 +0200

    Added URLrouter, favicon, JSON error response and chenged tabs into spaces.

--- GeoIPCache/source/app.d
+++ GeoIPCache/source/app.d
@@ -1,17 +1,38 @@
 import vibe.vibe;
+import std.conv;
 
 void main()
 {
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
+    auto router = new URLRouter;
+    router.get("/", &hello);
 
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-	runApplication();
+    //Favicon
+    auto faviconSettings = new HTTPFileServerSettings;
+    faviconSettings.serverPathPrefix = "/";
+    faviconSettings.options = HTTPFileServerOption.failIfNotFound;
+    faviconSettings.preWriteCallback = (req, res, ref path) {
+        res.headers["Content-Type"] = "image/vnd.microsoft.icon";
+    };
+    router.get("/favicon.ico", serveStaticFiles("public/", faviconSettings));
+
+    auto settings = new HTTPServerSettings;
+    settings.port = 8080;
+    settings.bindAddresses = ["0.0.0.0"];
+    settings.errorPageHandler = toDelegate(&errorPage);
+    listenHTTP(settings, router);
+
+    runApplication();
+}
+
+void errorPage(HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorInfo error)
+{
+    auto retvalArray = [ "error_code" : to!string(error.code) , "message" : to!string(error.message) ] ;
+    auto retvalJSON = serializeToJson(retvalArray);
+    res.headers["Content-Type"] = "application/json";
+    res.writeBody(retvalJSON.toString());
 }
 
 void hello(HTTPServerRequest req, HTTPServerResponse res)
 {
-	res.writeBody("Hello, World!");
+    res.writeBody("Hello, World!\n");
 }
